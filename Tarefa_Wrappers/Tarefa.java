7c0a744658fb39ca1868a093d07cb7697636c43a
Nicollas-Trovao <137838083+Nicollas-Trovao@users.noreply.github.com>
2023-08-20 21:21:07 -0300

    Add files via upload

---
+++ Tarefa_Wrappers/Tarefa.java
@@ -0,0 +1,13 @@
+public class Tarefa {
+
+    public static void main(String[] args){
+    int Numero = 3;
+    System.out.print(Numero);
+    
+    //Coversão para não primitivo
+    
+    Integer Numero1 = Integer.valueOf(Numero);
+    System.out.print (Numero1);
+
+    }
+}
